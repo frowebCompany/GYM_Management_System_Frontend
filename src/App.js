fdd4f157cfbf5a6f9fb5462541a40e0e7875feba
frowebCompany <frowebcompany@gmail.com>
2022-09-03 15:46:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="min-h-screen background_gradient"> 
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
